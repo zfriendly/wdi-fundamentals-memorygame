c5571e042166722cee7e92eb27b9443cfb174692
Zack Friendly <Zack@Zack-Friendly.local>
2020-01-09 17:33:57 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,21 @@
 
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+
+
+const cards = ['queen', 'queen', 'king', 'king'];
+const cardsInPlay = [];
+const cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log('User flipped '+cardsInPlay[0]);
+const cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log('User flipped '+cardsInPlay[1]);
+// console.log(cardsInPlay.length);
+if (cardsInPlay.length === 2) {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again.")
+
+}
+}
