88c213a9e86ad40227e31aed0165e5dcecfbc347
Zack Friendly <Zack@Zack-Friendly.local>
2020-01-10 15:30:35 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -3,19 +3,24 @@
 
 const cards = ['queen', 'queen', 'king', 'king'];
 const cardsInPlay = [];
-const cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log('User flipped '+cardsInPlay[0]);
-const cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log('User flipped '+cardsInPlay[1]);
-// console.log(cardsInPlay.length);
-if (cardsInPlay.length === 2) {
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
+
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			console.log("You found a match!");
+		}
+		else {
+			console.log("Sorry, try again.")
+		}
+	}
 }
-else {
-	alert("Sorry, try again.")
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
 
 }
-}
+
+flipCard(0);
+flipCard(2);
